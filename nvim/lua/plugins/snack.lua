8cfa0009d2af9dc2e72ccdf4dbdcdfccddb2fd41
Jhonathan Wolff <jhonathan.wolff@wppmediaservices.com.br>
2026-02-05 13:18:06 -0300

    removed function

--- nvim/lua/plugins/snack.lua
+++ nvim/lua/plugins/snack.lua
@@ -76,7 +76,7 @@ return {
     { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
     { "<leader>,", function() Snacks.picker.buffers() end, desc = "Buffers" },
     { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
-    { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
+    --{ "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
     { "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History" },
     { "<leader>o", function() Snacks.explorer() end, desc = "File Explorer" },
     -- find
