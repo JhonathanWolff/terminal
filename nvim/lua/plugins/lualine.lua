4f2ecf76e67744e84074ddfd222ee2666347f483
Jhonathan Wolff <jhonathan.wolff@wppmediaservices.com.br>
2026-02-13 16:58:13 -0300

    update lua line

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,7 +1,18 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-	    local line= require("lualine").setup()
-    end
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+						path = 1, -- Caminho relativo (recomendado)
+						file_status = true,
+						shorting_target = 40,
+					},
+				},
+			},
+		})
+	end,
 }
