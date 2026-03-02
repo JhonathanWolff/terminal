de02cbd71fb2fde08f6f11b9a0ff53f451d93923
Jhonathan Wolff <jhonathan.wolff@wppmediaservices.com.br>
2026-03-02 14:33:12 -0300

    update telescope config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,19 +1,35 @@
 return
-	{
-    'nvim-telescope/telescope.nvim', version = '*',
+{
+    'nvim-telescope/telescope.nvim',
+    version = '*',
     dependencies = {
         'nvim-lua/plenary.nvim',
         -- optional but recommended
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
-    config = function ()
-	    local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-		vim.keymap.set('n', '<leader>fw', builtin.live_grep, { desc = 'Telescope live grep' })
-		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+    config = function()
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+        vim.keymap.set('n', '<leader>fw', builtin.live_grep, { desc = 'Telescope live grep' })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
 
         -- folke todo
         vim.keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = "Open Telescope TODO List" })
+
+        local actions = require('telescope.actions')
+        require('telescope').setup {
+            defaults = {
+                mappings = {
+                    i = {                             -- Mappings for insert mode
+                        ["<M-q>"] = actions.send_to_qflist,
+                    },
+                    n = {                             -- Mappings for normal mode
+                        ["<M-q>"] = actions.send_to_qflist
+                    },
+                },
+            },
+        }
     end
 }
