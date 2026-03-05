8bd1a9ad6fccde3847ac49b8a4a391cfce3431fd
Jhonathan Wolff <jhonathan.wolff@wppmediaservices.com.br>
2026-03-05 17:29:08 -0300

    new code

---
+++ terminal_functions/javascript_functions.bash
@@ -0,0 +1,32 @@
+
+
+function node_install_dev()
+{
+    if [[ -f "package.json" ]]; then
+        npm install -D @types/node
+    fi
+}
+
+function ts_install_dev()
+{
+    if [[ -f "package.json" ]]; then
+        npm install -D @types/node
+    fi
+
+#Nao pode ter espaco no EOF
+    cat << EOF > tsconfig.json
+    {
+        "compilerOptions": {
+          "target": "ES2020",
+          "module": "commonjs",
+          "moduleResolution": "node",
+          "esModuleInterop": true,
+          "types": ["node"],
+          "strict": true,
+          "outDir": "./dist"
+        },
+        "include": ["src/**/*"],
+        "exclude": ["node_modules"]
+    }
+EOF
+}
