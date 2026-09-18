f002f20988d41230bc55aefc35385807542b3d59
Jhonathan Wolff <jhonathan.wolff@wppmediaservices.com.br>
2026-09-18 13:26:45 -0300

    comments

--- terminal_functions/functions.bash
+++ terminal_functions/functions.bash
@@ -93,8 +93,8 @@ function update_terminal {
   cd $TERMINAL
   lazygit
 
-  cd $HOME/.config/nvim
-  lazygit
+  #cd $HOME/.config/nvim
+  #lazygit
 
   # find . | grep -E "(/__pycache__$|\.pyc$|\.pyo$)" | xargs rm -rf
   # git add --all
